5d8ee06185e75d89d3a1a5c4748775afa94802e0
Никита Кондрашов <hasenberg41@mail.ru>
2022-05-29 15:28:37 +0300

    Добавил валидацию создания пользователя

--- CatalinaNetworks.API/Program.cs
+++ CatalinaNetworks.API/Program.cs
@@ -1,6 +1,8 @@
 using CatalinaNetworks.API.Profiles;
 using CatalinaNetworks.DataBase;
+using FluentValidation;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 //Say my name
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,8 @@ builder.Services.AddAutoMapper(cfg =>
     cfg.AddProfile<ApiUserMappingProfile>();
     cfg.AddProfile<DBUserMappingProfile>();
 });
+
+builder.Services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
 
 // Пока слои логики не реализованы, контроллер использует методы контекста
 // контекста базы данных напрямую для тестирования frontend части
---
+++ CatalinaNetworks.BusinessLogic/Validators/CreateUserValidator.cs
@@ -0,0 +1,18 @@
+﻿using CatalinaNetworks.Core.Models;
+using FluentValidation;
+
+namespace CatalinaNetworks.BusinessLogic.Validators
+{
+    public class CreateUserValidator : AbstractValidator<User>
+    {
+        public CreateUserValidator()
+        {
+            RuleFor(u => u.Id).Empty();
+            RuleFor(u => u.Name).NotEmpty().MaximumLength(100);
+            RuleFor(u => u.UniqueUrlName).MaximumLength(300);
+            RuleFor(u => u.Photos.Small).MaximumLength(50);
+            RuleFor(u => u.Photos.Large).MaximumLength(50);
+            RuleFor(u => u.Photos.UserId).Equal(u => u.Id);
+        }
+    }
+}
