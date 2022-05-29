3568ac8cb7b7d481d7682d0fa0de04c78731a0d5
Никита Кондрашов <hasenberg41@mail.ru>
2022-05-29 17:03:55 +0300

    Создал подключение к тестовой бд

---
+++ CatalinaNetworks.DataBase.Tests/TestDatabaseFixture.cs
@@ -0,0 +1,60 @@
+namespace CatalinaNetworks.DataBase.Tests
+{
+    public class TestDatabaseFixture
+    {
+        private const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=CatalinaNetworksDbTests;Trusted_Connection=True";
+        
+        private static readonly object _lock = new();
+        private static bool _databaseInitialized;
+
+        public TestDatabaseFixture()
+        {
+            lock (_lock)
+            {
+                if (!_databaseInitialized)
+                {
+                    using (var context = CreateContext())
+                    {
+                        context.Database.EnsureDeleted();
+                        context.Database.EnsureCreated();
+
+                        context.Users.AddRange(new Entities.User()
+                        {
+                            Name = "Валера",
+                            UniqueUrlName = Guid.NewGuid().ToString(),
+                            Photos = new Entities.Photos()
+                            {
+                                Small = Guid.NewGuid().ToString(),
+                                Large = Guid.NewGuid().ToString()
+                            }
+                        }, new Entities.User()
+                        {
+                            Name = "Жорик",
+                            UniqueUrlName = Guid.NewGuid().ToString(),
+                            Photos = new Entities.Photos()
+                            {
+                                Small = Guid.NewGuid().ToString(),
+                                Large = Guid.NewGuid().ToString()
+                            }
+                        }, new Entities.User()
+                        {
+                            Name = "Саня",
+                            UniqueUrlName = Guid.NewGuid().ToString(),
+                            Photos = new Entities.Photos()
+                            {
+                                Small = Guid.NewGuid().ToString(),
+                                Large = Guid.NewGuid().ToString()
+                            }
+                        });
+                    }
+                }
+            }
+        }
+
+        private UsersDbContext CreateContext() 
+            => new UsersDbContext(
+                new DbContextOptionsBuilder<UsersDbContext>()
+                .UseSqlServer(ConnectionString)
+                .Options);
+    }
+}
--- CatalinaNetworks.DataBase.Tests/Usings.cs
+++ CatalinaNetworks.DataBase.Tests/Usings.cs
@@ -1,1 +1,2 @@
+global using Microsoft.EntityFrameworkCore;
 global using Xunit;
