e47c6267ae6b76c6437670dc7e74ce87cb1ea996
Никита Кондрашов <hasenberg41@mail.ru>
2022-05-29 15:09:20 +0300

    Создал middleware для обработки исключений

---
+++ CatalinaNetworks.API/Middleware/CustomExceptionHandlerMiddleware.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+
+namespace CatalinaNetworks.API.Middleware
+{
+    internal class CustomExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceptionHandlerMiddleware(RequestDelegate next) => _next = next;
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(httpContext, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext httpContext, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            string result = string.Empty;
+            // TODO : доделать обработку исключений
+        }
+    }
+}
