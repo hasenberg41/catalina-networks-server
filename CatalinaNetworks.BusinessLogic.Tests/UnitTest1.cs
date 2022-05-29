29f78bf6faac4164c18ef33a50efa4b6f40ebbac
Никита Кондрашов <hasenberg41@mail.ru>
2022-05-29 16:30:56 +0300

    Init tests projects

---
+++ CatalinaNetworks.BusinessLogic.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CatalinaNetworks.BusinessLogic.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ CatalinaNetworks.DataBase.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CatalinaNetworks.DataBase.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
