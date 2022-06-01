22e8565e64e968d72660be67c231196e14871dc5
Никита Кондрашов <hasenberg41@mail.ru>
2022-06-01 09:34:38 +0300

    Добавил параметры запроса для паггинации списка пользователей

---
+++ CatalinaNetworks.Core/Models/Paggination/UserParameters.cs
@@ -0,0 +1,28 @@
+﻿namespace CatalinaNetworks.Core.Models.Paggination
+{
+    public class UserParameters
+    {
+        const int maxPageSize = 50;
+        private int _pageSize = 10;
+
+        public int PageNumber { get; set; } = 1;
+        public int PageSize
+        {
+            get => _pageSize; set
+            {
+                if (value <= 0)
+                {
+                    _pageSize = 1;
+                }
+                else if (value > maxPageSize)
+                {
+                    _pageSize = maxPageSize;
+                }
+                else
+                {
+                    _pageSize = value;
+                }
+            }
+        }
+    }
+}
